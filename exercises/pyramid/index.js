4f5f28bcb94d2229ae7cf4b0d698f3286a1996ff
Elar Saks <elarsaks@gmail.com>
2022-02-21 20:58:07 +0200

    Start wit Pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,73 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+/*
+function pyramid(n) {
+	let spaces = '';
+	let hashes = '';
+
+	for(let i = 1; i <= n - 1; i++){
+		spaces = spaces + ' '
+	}
+
+	for(let i = 1; i <= n ; i++){
+	
+
+		if(hashes.length < 1 ){
+			hashes = hashes + '#'
+		} else {
+			hashes = hashes + '##'
+			spaces = spaces.slice(0, spaces.length -1)
+		}				
+		console.log(spaces + hashes + spaces)
+	}
+} */
+
+/* Produces 1 extra space!
+function pyramid(n) {
+	const midpoint = Math.floor((2 * n -1) / 2)
+
+	for(let row = 0; row < n; row++){
+		let level = '';
+
+		for(let column = 0; column <= 2 * n - 1 ; column++){
+
+			if(midpoint - row <= column && midpoint + row >= column){
+				level += '#';
+			} else {
+				level += ' ';
+			}
+		}
+
+		console.log(level)
+	}
+} 
+*/ 
+
+function pyramid(n) {
+	const midpoint = Math.floor((2 * n -1) / 2)
+
+	for(let row = 0; row < n; row++){
+		let level = '';
+
+		for(let column = 0; column <= 2 * n - 1; column++){
+
+			if(midpoint - row <= column && midpoint + row >= column){
+				level += '#';
+			} else {
+				level += ' ';
+			}
+		}
+
+		console.log(level)
+	}
+} 
+
+pyramid(1)
+console.log('_______________')
+pyramid(2)
+console.log('_______________')
+pyramid(3)
+
 
 module.exports = pyramid;
